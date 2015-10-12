04e4b8063a353634c9e70a489b6b942408773f36
David Furnes <david@dfurnes.com>
2015-10-12 01:39:27 -0400

    Getting rid of Prezto.

--- zsh/README.md
+++ zsh/README.md
@@ -1,3 +1,1 @@
-[Zsh](http://www.zsh.org) is the shell that any _real_ developers use. Honestly if you dont you're not even trying.
-
-I use [Zprezto](https://github.com/sorin-ionescu/prezto) because it seems less bulky than Oh-My-Zsh and my shell no longer takes 30 seconds to load.
+I use [Zsh](http://www.zsh.org) with Sindre Sorhus' [Pure](https://github.com/sindresorhus/pure) prompt.
---
+++ zsh/bindings.zsh
@@ -0,0 +1,27 @@
+# Configure Vim mode:
+set -o vi
+
+# reduce delay on <ESC> to 0.1s
+export KEYTIMEOUT=1
+
+# keep backspace and ^h working even
+# after returning from command mode
+bindkey '^?' backward-delete-char
+bindkey '^h' backward-delete-char
+
+# Use ^w to remove word backwards
+bindkey '^w' backward-kill-word
+
+# Use ^z to quickly switch between Vim and the terminal.
+# <http://sheerun.net/2014/03/21/how-to-boost-your-vim-productivity/>
+fancy-ctrl-z () {
+  if [[ $#BUFFER -eq 0 ]]; then
+    BUFFER="fg"
+    zle accept-line
+  else
+    zle push-input
+    zle clear-screen
+  fi
+}
+zle -N fancy-ctrl-z
+bindkey '^Z' fancy-ctrl-z
