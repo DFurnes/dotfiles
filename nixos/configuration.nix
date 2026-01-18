56829334474d6ce973133e4c9731b991307ad92b
David Furnes <david@dfurnes.com>
2026-01-18 13:25:29 -0500

    Enable Flatpak & fix conflict with GPG config.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -82,6 +82,9 @@
   programs.zsh.enable = true;
   programs.firefox.enable = true;
 
+  # Flatpak:
+  services.flatpak.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
