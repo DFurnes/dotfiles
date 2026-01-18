fe6db7e40041f28b7655d2ed11b3c175cb5642c8
David Furnes <david@dfurnes.com>
2026-01-18 13:14:44 -0500

    Cleanup.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -9,31 +9,25 @@
     ../modules/system-packages.nix
   ];
 
-  # Bootloader.
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Nix "experimental" features:
+  nix.settings.experimental-features = [ "flakes" "nix-command" ];
+
+  # Bootloader:
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.timeout = 0;
 
-  # Nix "experimental" features:
-  nix.settings.experimental-features = [ "flakes" "nix-command" ];
-
+  # Networking:
   networking.hostName = "desktop";
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
+  # Time zone & i18n:
   time.timeZone = "America/New_York";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
@@ -46,10 +40,8 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
+  # Desktop environment:
   services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
@@ -70,16 +62,7 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.dfurnes = {
@@ -95,13 +78,9 @@
     ];
   };
    
+  # System programs:
   programs.zsh.enable = true;
-   
-  # Install firefox.
   programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -113,45 +92,19 @@
     unzip
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  #   pinentryPackage = pkgs.pinentry-gnome3;
-  # };
-
   # List services that you want to enable:
   programs.npm.enable = true;
   programs.npm.npmrc = ''
     prefix = ''${HOME}/.npm
   '';
 
-  # Use GNOME Keyring for secrets, but NOT as the SSH agent.
-  # services.gnome.gnome-keyring.enable = true;
-  # services.gnome.gcr-ssh-agent.enable = false;
-  # programs.seahorse.enable = true;
-  # services.dbus.packages = [ pkgs.gnome-keyring pkgs.gcr ];
-
-  # Make PAM unlock/start the keyring on login (GDM)
-  # security.pam.services.gdm.enableGnomeKeyring = true;
-  # security.pam.services.gdm-password.enableGnomeKeyring = true;
-
-  # Don’t run a competing OpenSSH agent
-  # programs.ssh.startAgent = false;
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  # OpenSSH server & Avahi:
   services.openssh.enable = true;
   services.openssh.settings = {
     PermitRootLogin = "no";
     PasswordAuthentication = false;
     KbdInteractiveAuthentication = false;
   };
-
-  # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 22 ];
 
   # Avahi:
   services.avahi = {
@@ -162,12 +115,10 @@
     publish.userServices = true;
   };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [ 22 ];
 
+  # Default settings for stateful data (e.g. file locations or database versions),
+  # based on first install. Do not change without validating compatibility!
+  system.stateVersion = "25.11";
 }
