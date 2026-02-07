0296d1d7bf76d21f08522c468b6d8d0fdb1e41a7
David Furnes <david@dfurnes.com>
2026-02-07 10:49:18 -0500

    Configure bindings for Forge.

--- home/flake.nix
+++ home/flake.nix
@@ -72,12 +72,52 @@
             "forge@jmmaranan.com"
           ];
         };
+        
+        "org/gnome/shell/keybindings" = {
+          switch-to-application-1 = [ ]; # Conflict: Super+1
+          switch-to-application-2 = [ ]; # Conflict: Super+2
+          switch-to-application-3 = [ ]; # etc.
+          switch-to-application-4 = [ ];
+          switch-to-application-5 = [ ];
+          switch-to-application-6 = [ ];
+          switch-to-application-7 = [ ];
+          switch-to-application-8 = [ ];
+          switch-to-application-9 = [ ];
+        };
+
+        "org/gnome/desktop/wm/keybindings" = {
+          hide = [ ]; # Conflict: Super+H
+          tile-left  = [ ]; # Conflict: Super+Left
+          tile-right = [ ]; # Conflict: Super+Right
+
+          switch-to-workspace-1 = [ "<Super>1" ];
+          switch-to-workspace-2 = [ "<Super>2" ];
+          switch-to-workspace-3 = [ "<Super>3" ];
+          switch-to-workspace-4 = [ "<Super>4" ];
+          switch-to-workspace-5 = [ "<Super>5" ];
+          switch-to-workspace-6 = [ "<Super>6" ];
+
+          move-to-workspace-1 = [ "<Shift><Super>1" ];
+          move-to-workspace-2 = [ "<Shift><Super>2" ];
+          move-to-workspace-3 = [ "<Shift><Super>3" ];
+          move-to-workspace-4 = [ "<Shift><Super>4" ];
+          move-to-workspace-5 = [ "<Shift><Super>5" ];
+          move-to-workspace-6 = [ "<Shift><Super>6" ];
+        };
+
+        "org/gnome/mutter" = {
+          dynamic-workspaces = false;
+        };
+
+        "org/gnome/desktop/wm/preferences" = {
+          num-workspaces = 6;
+        };
       };
     };
-  in
-  {
-    users = {
-      dfurnes = userConfig;
+    in
+    {
+      users = {
+        dfurnes = userConfig;
+      };
     };
-  };
 }
